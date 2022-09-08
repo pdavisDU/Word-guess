b99cc5f7c67954a3e55db969f2bd1abdf1ab285b
pdavisDU <pdavisdu@gmail.com>
2022-09-07 18:32:29 -0600

    adding query selectors

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ var wordArray = [
 ];
 
 var wordInput = document.querySelector("#word");
+var startButton = document.querySelector("#start_button");
+var timerEl = document.querySelector("#timerCount");
+var win = document.querySelector("#wins");
+var lose = document.querySelector("#loses");
+var wordBlank = document.querySelector("#result_msg");
+var resetButton = document.querySelector("#reset_button");
