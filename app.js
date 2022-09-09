64aa3dc4e910a02f7900e966dbea4f83576aea3b
pdavisDU <pdavisdu@gmail.com>
2022-09-09 16:19:38 -0600

    adding win/loss count and timer

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ var win = document.querySelector("#wins");
 var lose = document.querySelector("#loses");
 var wordBlank = document.querySelector("#result_msg");
 var resetButton = document.querySelector("#reset_button");
+
+var winCounter = 0;
+var loseCounter = 0;
+var timerCount = 10;
